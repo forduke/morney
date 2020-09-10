f75e098b68e7996418a1a0275f2595dc7001fb19
forduke <forduke@163.com>
2020-09-11 01:46:15 +0800

    使用 v-model 代替 :value 和 @input 绑定

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -3,8 +3,7 @@
     <label class="notes">
       <span class="name">备注</span>
       <input type="text"
-             :value="value"
-             @change="onInput"
+             v-model="value"
              placeholder="在这里输入备注">
     </label>
   </div>
@@ -13,15 +12,10 @@
 <script lang="ts">
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
-
+  
   @Component
   export default class Notes extends Vue {
     value = '';
-    
-    onInput(event: KeyboardEvent) {
-      const input = event.target as HTMLInputElement;
-      this.value = input.value;
-    }
   }
 </script>
 
